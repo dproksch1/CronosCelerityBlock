4ca1272e37ea2c366259a36c4a85c8928fff4201
Daniel Proksch <daniel@proksch-family.de>
2022-04-16 17:36:09 +0200

    added test output to track error timing in logfile

--- cronos_block/kernel/singlestep_block.C
+++ cronos_block/kernel/singlestep_block.C
@@ -348,8 +348,9 @@ double HyperbolicSolver::singlestep(Data &gdata, gridFunc &gfunc,
 				const int fluidType = Riemann[DirX]->get_Fluid_Type();
 
 				if(ix >= 0 && ix <= gdata.mx[0] && iy >= 0 && iy <= gdata.mx[1] && iz >= 0 && iz <= gdata.mx[2]) {
-
+					cout << "computeStep started\n";
 					const auto numVals = computeStep(reconst, Trafo, PhysFlux, Riemann, Problem, eos, gdata, ix, iy, iz, max_cfl_lin);
+					cout << "computeStep completed\n";
 				}
 			});
 		});
