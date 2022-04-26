26cb91db4823ffed2573195783a20bf571cbdd46
Daniel Proksch <daniel@proksch-family.de>
2022-04-26 14:31:19 +0200

    fixed slight syntax mistake

--- cronos_block/kernel/singlestep_block.C
+++ cronos_block/kernel/singlestep_block.C
@@ -349,9 +349,9 @@ cout << "begin queue in singlestep" << endl;
 				if (ix == ixEnd && iy == gdata.mx[1]+n_ghost[1]-1 && iz == gdata.mx[2]+n_ghost[2]-1) cout << "reach limit: " << ix << "." <<  iy << "." << iz << "\n";
 				const int fluidType = Riemann[DirX]->get_Fluid_Type();
 
-				if(ix >= 0 && ix <= gdata.mx[0] && iy >= 0 && iy <= gdata.mx[1] && iz >= 0 && iz <= gdata.mx[2]) {
+				/*if(ix >= 0 && ix <= gdata.mx[0] && iy >= 0 && iy <= gdata.mx[1] && iz >= 0 && iz <= gdata.mx[2]) {
 					const auto numVals = computeStep(reconst, Trafo, PhysFlux, Riemann, Problem, eos, gdata, ix, iy, iz, max_cfl_lin);
-				}
+				}*/
 			});
 		});
 	}
